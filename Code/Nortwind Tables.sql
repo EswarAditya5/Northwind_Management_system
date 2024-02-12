5cf4a22a4211cda4d800249736b6cb67b845bc17
Eswar Aditya <142793639+EswarAditya5@users.noreply.github.com>
2024-02-12 11:25:51 +0530

    Northwind Tables

--- Code/Nortwind Tables.sql
+++ Code/Nortwind Tables.sql
@@ -30,3 +30,151 @@ CREATE TABLE `categories` (
   PRIMARY KEY (`CategoryID`),
   UNIQUE KEY `CategoryName` (`CategoryName`)
 ) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `customers`
+-- ----------------------------
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE `customers` (
+  `CustomerID` varchar(5) NOT NULL,
+  `CompanyName` varchar(40) NOT NULL,
+  `ContactName` varchar(30) DEFAULT NULL,
+  `ContactTitle` varchar(30) DEFAULT NULL,
+  `Address` varchar(60) DEFAULT NULL,
+  `City` varchar(15) DEFAULT NULL,
+  `Region` varchar(15) DEFAULT NULL,
+  `PostalCode` varchar(10) DEFAULT NULL,
+  `Country` varchar(15) DEFAULT NULL,
+  `Phone` varchar(24) DEFAULT NULL,
+  `Fax` varchar(24) DEFAULT NULL,
+  `Image` longblob,
+  `ImageThumbnail` longblob,
+  PRIMARY KEY (`CustomerID`),
+  KEY `City` (`City`),
+  KEY `CompanyName` (`CompanyName`),
+  KEY `PostalCode` (`PostalCode`),
+  KEY `Region` (`Region`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `employees`
+-- ----------------------------
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE `employees` (
+  `EmployeeID` int(11) NOT NULL AUTO_INCREMENT,
+  `LastName` varchar(20) NOT NULL,
+  `FirstName` varchar(10) NOT NULL,
+  `Title` varchar(30) DEFAULT NULL,
+  `TitleOfCourtesy` varchar(25) DEFAULT NULL,
+  `BirthDate` datetime DEFAULT NULL,
+  `HireDate` datetime DEFAULT NULL,
+  `Address` varchar(60) DEFAULT NULL,
+  `City` varchar(15) DEFAULT NULL,
+  `Region` varchar(15) DEFAULT NULL,
+  `PostalCode` varchar(10) DEFAULT NULL,
+  `Country` varchar(15) DEFAULT NULL,
+  `HomePhone` varchar(24) DEFAULT NULL,
+  `Extension` varchar(4) DEFAULT NULL,
+  `Photo` longblob,
+  `Notes` longtext,
+  `ReportsTo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`EmployeeID`),
+  KEY `LastName` (`LastName`),
+  KEY `PostalCode` (`PostalCode`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `order details`
+-- ----------------------------
+DROP TABLE IF EXISTS `order details`;
+CREATE TABLE `order details` (
+  `OrderID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `UnitPrice` decimal(19,4) NOT NULL DEFAULT '0.0000',
+  `Quantity` int(11) NOT NULL DEFAULT '1',
+  `Discount` float NOT NULL DEFAULT '0',
+  PRIMARY KEY (`OrderID`,`ProductID`),
+  KEY `OrderID` (`OrderID`),
+  KEY `ProductID` (`ProductID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `orders`
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL AUTO_INCREMENT,
+  `CustomerID` varchar(5) DEFAULT NULL,
+  `EmployeeID` int(11) DEFAULT NULL,
+  `OrderDate` datetime DEFAULT NULL,
+  `RequiredDate` datetime DEFAULT NULL,
+  `ShippedDate` datetime DEFAULT NULL,
+  `ShipVia` int(11) DEFAULT NULL,
+  `Freight` decimal(19,4) DEFAULT '0.0000',
+  `ShipName` varchar(40) DEFAULT NULL,
+  `ShipAddress` varchar(60) DEFAULT NULL,
+  `ShipCity` varchar(15) DEFAULT NULL,
+  `ShipRegion` varchar(15) DEFAULT NULL,
+  `ShipPostalCode` varchar(10) DEFAULT NULL,
+  `ShipCountry` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`OrderID`),
+  KEY `CustomerID` (`CustomerID`),
+  KEY `EmployeeID` (`EmployeeID`),
+  KEY `OrderDate` (`OrderDate`),
+  KEY `ShippedDate` (`ShippedDate`),
+  KEY `ShipPostalCode` (`ShipPostalCode`)
+) ENGINE=MyISAM AUTO_INCREMENT=11078 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `products`
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `ProductID` int(11) NOT NULL AUTO_INCREMENT,
+  `ProductName` varchar(40) NOT NULL,
+  `SupplierID` int(11) DEFAULT NULL,
+  `CategoryID` int(11) DEFAULT NULL,
+  `QuantityPerUnit` varchar(20) DEFAULT NULL,
+  `UnitPrice` decimal(19,4) DEFAULT '0.0000',
+  `UnitsInStock` int(11) DEFAULT '0',
+  `UnitsOnOrder` int(11) DEFAULT '0',
+  `ReorderLevel` int(11) DEFAULT '0',
+  `Discontinued` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`ProductID`),
+  KEY `CategoryID` (`CategoryID`),
+  KEY `ProductName` (`ProductName`),
+  KEY `SupplierID` (`SupplierID`)
+) ENGINE=MyISAM AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `shippers`
+-- ----------------------------
+DROP TABLE IF EXISTS `shippers`;
+CREATE TABLE `shippers` (
+  `ShipperID` int(11) NOT NULL AUTO_INCREMENT,
+  `CompanyName` varchar(40) NOT NULL,
+  `Phone` varchar(24) DEFAULT NULL,
+  PRIMARY KEY (`ShipperID`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for `suppliers`
+-- ----------------------------
+DROP TABLE IF EXISTS `suppliers`;
+CREATE TABLE `suppliers` (
+  `SupplierID` int(11) NOT NULL AUTO_INCREMENT,
+  `CompanyName` varchar(40) NOT NULL,
+  `ContactName` varchar(30) DEFAULT NULL,
+  `ContactTitle` varchar(30) DEFAULT NULL,
+  `Address` varchar(60) DEFAULT NULL,
+  `City` varchar(15) DEFAULT NULL,
+  `Region` varchar(15) DEFAULT NULL,
+  `PostalCode` varchar(10) DEFAULT NULL,
+  `Country` varchar(15) DEFAULT NULL,
+  `Phone` varchar(24) DEFAULT NULL,
+  `Fax` varchar(24) DEFAULT NULL,
+  `HomePage` longtext,
+  PRIMARY KEY (`SupplierID`),
+  KEY `CompanyName` (`CompanyName`),
+  KEY `PostalCode` (`PostalCode`)
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
